1a47b6596221c75ca036c797e683d6f060651f54
jian <jianwang400@gmail.com>
2019-08-14 16:42:03 -0500

    Feat: add new Like function to the web

--- ins/admin.py
+++ ins/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from ins.models import Post, InsUser
+from ins.models import Post, InsUser, Like
 admin.site.register(Post)
 admin.site.register(InsUser)
+admin.site.register(Like)
