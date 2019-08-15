76dc5a2f90f14827f5e3beee8eac4bd440e8003f
jian <jianwang400@gmail.com>
2019-08-15 00:20:22 -0500

    Feat: click on the heart to add or cancel a like

--- ins/urls.py
+++ ins/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from ins.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from ins.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, addLike
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='helloworld'),
@@ -23,5 +23,6 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
     path('post/new/', PostCreateView.as_view(), name = 'make_post'),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name = 'post_update'),
-    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete')
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('like', addLike, name='addLike')
 ]
--- ins/views.py
+++ ins/views.py
@@ -1,6 +1,7 @@
+from annoying.decorators import ajax_request
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from ins.models import Post
+from ins.models import Post, Like
 from django.urls import reverse, reverse_lazy
 from ins.forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -36,3 +37,21 @@ class SignUp(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
     success_url = reverse_lazy("login")
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
